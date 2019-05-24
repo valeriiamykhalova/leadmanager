673dab678de66f24131076ebe245ec37fcb6c720
Lera <valeriya.mihaleva.1999@gmail.com>
2019-05-24 14:54:59 +0300

    Initial commit

---
+++ src/core/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path, include, re_path
+
+urlpatterns = [
+    path('', include('frontend.urls')),
+    re_path(r'', include(('leads.urls', 'leads'), namespace='leads')),
+]
---
+++ src/frontend/src/components/leads/Dashboard.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import From from './Form';
+import Leads from './Leads';
+
+export default function Dashboard() {
+    return (
+        <Fragment>
+            <From />
+            <Leads />
+        </Fragment>
+    )
+}
